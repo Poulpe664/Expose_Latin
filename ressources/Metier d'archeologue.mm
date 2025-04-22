46d29171816986fcbff1f364105488d5d7303346
Poulpe664 <noelepoulpe@proton.me>
2025-04-22 14:15:58 +0200

    add texts page and the mind map (no acess)

---
+++ ressources/Metier d'archeologue.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#000000" CREATED="1745319549625" ID="ID_1771064859" MODIFIED="1745319921189" TEXT="Metier d&apos;archeologue">
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+<node CREATED="1745319639925" ID="ID_1565138046" MODIFIED="1745319864100" POSITION="right" TEXT="Qu&apos;est ce que c&apos;est &#xea;tre archeologue ?"/>
+<node CREATED="1745319932564" ID="ID_1883883728" MODIFIED="1745319997749" POSITION="right" TEXT="Que fait l&apos;archeologue ?">
+<node CREATED="1745320135985" ID="ID_80566988" MODIFIED="1745320163112" TEXT="sur les zones de fouilles"/>
+<node CREATED="1745320163687" ID="ID_1897122108" MODIFIED="1745320182951" TEXT="en laboratoire"/>
+</node>
+<node CREATED="1745321352277" ID="ID_227788720" MODIFIED="1745321352277" POSITION="right" TEXT=""/>
+</node>
+</map>
