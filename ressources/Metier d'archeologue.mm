45bd978347ff5db7b564fcd2331b5d05066f38b1
Poulpe664 <noelepoulpe@proton.me>
2025-04-22 14:55:29 +0200

    Add files via upload

--- ressources/Metier d'archeologue.mm
+++ ressources/Metier d'archeologue.mm
@@ -9,16 +9,78 @@
 </node>
 <node CREATED="1745321352277" ID="ID_227788720" MODIFIED="1745324764994" POSITION="right" TEXT="Specialit&#xe9;s"/>
 <node CREATED="1745324776230" ID="ID_1270730134" MODIFIED="1745324778387" POSITION="right" TEXT="M&#xe9;thodes d&apos;&#xe9;tudes"/>
-<node CREATED="1745325116428" ID="ID_1672145582" MODIFIED="1745325390156" POSITION="right" TEXT="Post&#xe9;rit&#xe9; et image publique">
-<node CREATED="1745325122814" ID="ID_894624615" MODIFIED="1745325155261" TEXT="Films, series"/>
-<node CREATED="1745325137699" ID="ID_1486417926" MODIFIED="1745325145376" TEXT="Jeux"/>
-<node CREATED="1745325157247" ID="ID_1540904792" MODIFIED="1745325170049" TEXT="Livres"/>
+<node CREATED="1745325116428" ID="ID_1672145582" MODIFIED="1745326231565" POSITION="right" TEXT="Post&#xe9;rit&#xe9; et image publique">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <h1>
+      Voire <a href="https://poulpe664.github.io/Expose_Latin/ressources/textes.html">textes</a>
+    </h1>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="attach"/>
+<node CREATED="1745325122814" ID="ID_894624615" MODIFIED="1745325733213" TEXT="Films, series">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <h3>
+      EX: Indiana Jones
+    </h3>
+  </body>
+</html>
+</richcontent>
 </node>
-<node CREATED="1745325214865" ID="ID_1521402198" MODIFIED="1745325259894" POSITION="right" TEXT="Comment devenir archeologue ?">
+<node CREATED="1745325137699" ID="ID_1486417926" MODIFIED="1745325806083" TEXT="Jeux">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <h3>
+      EX: Tomb Raider
+    </h3>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1745325157247" ID="ID_1540904792" MODIFIED="1745325844297" TEXT="Livres">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <h3>
+      EX: Meurtre en M&#233;sopotamie
+    </h3>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1745325214865" ID="ID_1521402198" MODIFIED="1745326206653" POSITION="right" TEXT="Comment devenir archeologue ?">
 <node CREATED="1745325263091" ID="ID_778822859" MODIFIED="1745325269028" TEXT="Etudes"/>
 <node CREATED="1745325269537" ID="ID_1626639808" MODIFIED="1745325375271" TEXT="Capacites requises">
 <icon BUILTIN="help"/>
 </node>
+</node>
+<node CREATED="1745325536899" ID="ID_932767829" MODIFIED="1745325649734" POSITION="right" TEXT="Histoire de l&apos;arch&#xe9;ologie">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <h1>
+      Potentiellement long
+    </h1>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="messagebox_warning"/>
 </node>
 </node>
 </map>
